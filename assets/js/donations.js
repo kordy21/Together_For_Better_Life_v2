9b919da958b5f24b9435a7489d54b40e96e326d1
mohamedroauf <raoufmohamed019@gmail.com>
2025-08-21 14:56:16 +0300

    Raouf--M3aan --1--21/8

--- assets/js/donations.js
+++ assets/js/donations.js
@@ -5,10 +5,27 @@
       });
     });
 
-    // تفعيل اختيار مبلغ التبرع
     document.querySelectorAll('.donate-amount-option').forEach(amount => {
       amount.addEventListener('click', () => {
         document.querySelectorAll('.donate-amount-option').forEach(a => a.classList.remove('active'));
         amount.classList.add('active');
       });
     });
+document.addEventListener("DOMContentLoaded", function () {
+  const btn = document.getElementById("other-amount-btn");
+  const inputDiv = document.getElementById("other-amount-input");
+
+  btn.addEventListener("click", function (e) {
+    inputDiv.classList.remove("d-none");
+    e.stopPropagation(); 
+  });
+
+
+  inputDiv.addEventListener("click", function (e) {
+    e.stopPropagation();
+  });
+
+  document.addEventListener("click", function () {
+    inputDiv.classList.add("d-none");
+  });
+});
